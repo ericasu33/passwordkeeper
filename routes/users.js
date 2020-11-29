2f096036765afbc69444c2586e968ea00ff89b4b
mohdtorabi <mitq8.mt@gmail.com>
2020-11-29 06:15:20 +0000

    added registrations.js but did not add anything to server.js yet

--- routes/users.js
+++ routes/users.js
@@ -43,35 +43,7 @@ module.exports = (db) => {
     res.send({});
   });
 
-  /* const addUser =  function(user) {
-    const data = [user.name, user.email, user.password];
-    db.query(`
-    INSERT INTO users(name, email, password)
-    VALUES ($1, $2, $3) RETURNING *;
-    `, data)
-      .then(res => {
-        if (res.rows[0].name || res.rows[0].email) {
-          return res.rows[0];
-        } else {
-          return null;
-        }
-
-      });
-  };
-
-  router.get("/register", (req, res) => {
-    const user = req.body;
-    addUser(user)
-      .then(user => {
-        if (!user) {
-          res.send({error: "error"});
-          return;
-        }
-        req.session.userId = user.id;
-        res.send("🤗");
-      });
-    res.render("urls_register", templateVars);
-  });
+  /*
  */
 
 
