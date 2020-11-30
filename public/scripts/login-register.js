817461dfb581fd82e7c470c2981fbd935ab20df4
mohdtorabi <mitq8.mt@gmail.com>
2020-11-30 20:18:17 +0000

    partial/header is now working

---
+++ public/scripts/login-register.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('.nav-button').on('click', () => {
+    const x = document.getElementById("login");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    window.location = '#login';
+  });
+  myFunction();
+});
