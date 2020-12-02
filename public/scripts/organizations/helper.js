4aa5ecec075b6de48bed24c47249082c8d7f355d
mohdtorabi <mitq8.mt@gmail.com>
2020-12-02 01:11:15 +0000

    setting page styling is done up until add user table

--- public/scripts/organizations/helper.js
+++ public/scripts/organizations/helper.js
@@ -7,6 +7,7 @@ const isUrl = str => {
   }
 };
 
+
 const isEmail = email => {
   const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (email.match(mailformat)) {
