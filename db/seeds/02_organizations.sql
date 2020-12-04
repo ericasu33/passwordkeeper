4dff56cd1814122bedf0f63c0230b34ab95485f2
mohdtorabi <mitq8.mt@gmail.com>
2020-12-04 04:20:20 +0000

    database is updated

--- db/seeds/02_organizations.sql
+++ db/seeds/02_organizations.sql
@@ -1,4 +1,4 @@
-INSERT INTO organizations (name, logo_url) VALUES ('Lighthouse-labs', 'logo_url.png');
-INSERT INTO organizations (name, logo_url) VALUES ('Google', 'logo_google.png');
-INSERT INTO organizations (name, logo_url) VALUES ('BC-health', 'logo_BC-health.png');
-INSERT INTO organizations (name, logo_url) VALUES ('passwordkeepers', 'logo_passwordkeepers.png');
+INSERT INTO organizations (name, logo_url) VALUES ('Lighthouse-labs', 'https://www.lighthouselabs.ca/assets/lighthouselabs-logo-d5df6d383f7466b06a64de9561fd537493502eafe78bbf3bcd3de82884e5dd85.png');
+INSERT INTO organizations (name, logo_url) VALUES ('Google', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png');
+INSERT INTO organizations (name, logo_url) VALUES ('Github', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
+INSERT INTO organizations (name, logo_url) VALUES ('Facebook', 'https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512');
